f9c082721b6aa5994bd0e7d7434090491ee3aa32
Long <longshare9201@gmail.com>
2026-01-15 20:53:51 +0700

    fix timezone

--- src/common/interceptors/timezone.interceptor.ts
+++ src/common/interceptors/timezone.interceptor.ts
@@ -7,7 +7,6 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { formatInTimeZone } from 'date-fns-tz';
-import { Decimal } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class TimezoneInterceptor implements NestInterceptor {
@@ -16,30 +15,8 @@ export class TimezoneInterceptor implements NestInterceptor {
     }
 
     private transformData(data: any): any {
-        if (data === null || data === undefined) {
-            return data;
-        }
-
         if (data instanceof Date) {
             return formatInTimeZone(data, 'Asia/Ho_Chi_Minh', "yyyy-MM-dd'T'HH:mm:ss.SSS");
-        }
-
-        if (data instanceof Decimal) {
-            return data.toNumber();
-        }
-
-        if (Array.isArray(data)) {
-            return data.map((item) => this.transformData(item));
-        }
-
-        if (typeof data === 'object') {
-            const newData = {};
-            for (const key in data) {
-                if (Object.prototype.hasOwnProperty.call(data, key)) {
-                    newData[key] = this.transformData(data[key]);
-                }
-            }
-            return newData;
         }
 
         return data;
