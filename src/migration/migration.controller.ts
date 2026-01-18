d3258f6b90853f30bf0759369e978778dbb3e3f8
Long <longshare9201@gmail.com>
2026-01-18 15:28:59 +0700

    fix migrate

--- src/migration/migration.controller.ts
+++ src/migration/migration.controller.ts
@@ -1,12 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { MigrationService } from './migration.service';
 import { CloneDbDto } from './dto/clone-db.dto';
 
+@ApiTags('Migration')
 @Controller('migration')
 export class MigrationController {
     constructor(private readonly migrationService: MigrationService) { }
 
     @Post('clone')
+    @ApiOperation({ summary: 'Clone data from source DB' })
+    @ApiBody({ type: CloneDbDto })
     async cloneData(@Body() dto: CloneDbDto) {
         return this.migrationService.cloneData(dto);
     }
